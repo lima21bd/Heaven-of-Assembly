6c59d94240512b19c4bafc0c7ee501455142ddac
Khadija Akter Lima <38761302+lima21bd@users.noreply.github.com>
2019-05-01 12:34:10 +0600

    Add files via upload

---
+++ 7.asm
@@ -0,0 +1,66 @@
+.model small
+.stack 100h 
+
+.data
+a db 'ENTER THREE INITIALS: $' 
+b db ?
+
+.code 
+
+main proc 
+    
+    mov ax , @data
+    mov ds , ax
+    
+    mov ah , 9
+    lea dx , a
+    int 21h
+       
+    mov ah , 1
+    int 21h
+    mov bh , al 
+    
+    mov ah , 1
+    int 21h
+    mov bl , al
+    
+    mov ah , 1
+    int 21h
+    mov b , al
+    
+    mov ah , 2
+    mov dl , 10
+    int 21h
+    mov dl , 13
+    int 21h 
+    
+    mov ah , 2
+    mov dl , bh
+    int 21h 
+     
+    mov ah , 2
+    mov dl , 10
+    int 21h
+    mov dl , 13
+    int 21h 
+    
+    mov ah , 2
+    mov dl , bl
+    int 21h 
+    
+    mov ah , 2
+    mov dl , 10
+    int 21h
+    mov dl , 13
+    int 21h 
+    
+    mov ah , 2
+    mov dl , b
+    int 21h 
+      
+    exit:
+    
+    mov ah , 4ch
+    int 21h
+    main endp
+end main
